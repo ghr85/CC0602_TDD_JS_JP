f501901603bec8172a96672af3359ddecc8844b8
ghr85 <ghrenfrew@gmail.com>
2019-02-19 17:56:04 +0000

    added name, price and empty array properties to park object

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (name, price) {
+  this.name_str = name;
+  this.price_int = price;
+  this.dino_ary = [];
+}
+
+module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -3,18 +3,37 @@ const Park = require('../models/park.js');
 const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
-
+  let park
+  let dino
   beforeEach(function () {
-
+    park = new Park('Brassic Park',15)
+    dinosaur = new Dinosaur('t-rex', 'carnivore', 50);
   })
 
-  it('should have a name');
+  it('should have a name',function(){
+    const actual = park.name_str;
+    const expected = 'Brassic Park';
+    assert.strictEqual(actual,expected);
+  });
+  it('should have a ticket price',function(){
+    const actual = park.price_int;
+    const expected = 15;
+    assert.strictEqual(actual,expected);
+  });
+  it('should have a collection of dinosaurs',function(){
 
-  it('should have a ticket price');
+    const actual = park.dino_ary.length;
+    const expected = 0;
+    assert.strictEqual(actual,expected);
+  });
+  it('should be able to add a dinosaur to its collection',function(){
+    park.add_dino(dinsoaur);
+    const actual = park.dino_ary.length;
+    const expected = 1;
+    assert.strictEqual(actual,expected);
+  });
 
-  it('should have a collection of dinosaurs');
 
-  it('should be able to add a dinosaur to its collection');
 
   it('should be able to remove a dinosaur from its collection');
 
